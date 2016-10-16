74463c9f0d4c2d12ea73943f2c8ce47601cf2ab7
ttlexa <aleksey.grechka.it@gmail.com>
2016-10-16 07:50:01 +0300

    change: app/chat.js, views/pages/chat.ejs

--- app/chat.js
+++ app/chat.js
@@ -2,11 +2,19 @@
 
 "use strict"
 // TEST - socket.io
-module.exports = function(io){
-	io.on('connection', function (socket) {
-	  socket.emit('news', { hello: 'world' });
-	  socket.on('my other event', function (data) {
-	    console.log(data);
-	  });
-	});	
+// module.exports = function(io){
+// 	io.on('connection', function (socket) {
+// 	  socket.emit('news', { hello: 'world' });
+// 	  socket.on('my other event', function (data) {
+// 	    console.log(data);
+// 	  });
+// 	});	
+// };
+
+var User = require('./models/user');
+module.exports = function (io) {
+io.on('connection', function (socket) {
+	console.log(User);
+	console.log('connect 111');
+});
 };
