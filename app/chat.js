ab9c662ee18a40452b7ee45424b19b2a2f1ad33b
ttlexa <aleksey.grechka.it@gmail.com>
2016-10-20 01:05:55 +0300

    create chat without Auth

--- app/chat.js
+++ app/chat.js
@@ -1,20 +1,14 @@
 // app/chat.js
 
 "use strict"
-// TEST - socket.io
-// module.exports = function(io){
-// 	io.on('connection', function (socket) {
-// 	  socket.emit('news', { hello: 'world' });
-// 	  socket.on('my other event', function (data) {
-// 	    console.log(data);
-// 	  });
-// 	});	
-// };
 
-var User = require('./models/user');
 module.exports = function (io) {
 io.on('connection', function (socket) {
-	console.log(User);
-	console.log('connect 111');
+	
+	socket.on('message', function(text, cb){
+		socket.broadcast.emit('message', text);
+		cb();
+	});
+
 });
 };
